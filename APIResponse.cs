82c2fc4ec6ae841bb7323ae11ac586603b1b96fa
Galeano Folle Domingues <88506164+TakatJJ@users.noreply.github.com>
2024-02-01 14:54:23 -0300

    Creation of the project.
    Structured the Stocks data and managed the API.

---
+++ APIResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketGuide
+{
+    internal class APIResponse
+    {
+        public List<StocksINFO> Results { get; set; }
+        public DateTime RequestedAt { get; set; }
+        public string Took { get; set; }
+    }
+}
---
+++ UserClauses.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketGuide
+{
+    internal class UserClauses
+    {
+        public double SellClause {  get; }
+        public double BuyClause { get; }
+
+        public UserClauses(string[] args)
+        {
+            SellClause = double.Parse(args[1], CultureInfo.InvariantCulture);
+            BuyClause = double.Parse(args[2], CultureInfo.InvariantCulture);
+        }
+
+        public bool CheckClauses(double price)
+        {
+            if (BuyClause > price)
+            {
+                Console.WriteLine("Enviar email BUY");
+            }
+
+            if (SellClause < price)
+            {
+                Console.WriteLine("Enviar email SELL");
+            }
+            return true;
+        }
+    }
+}
