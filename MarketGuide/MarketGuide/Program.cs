1553625ffe95d274075c238fd17c484072c1cb64
Galeano Folle Domingues <galeanodomingues@gmail.com>
2024-02-01 14:52:40 -0300

    Creation of the project.
    Adding the functionalities of API Request and structure of the Stocks data.

---
+++ MarketGuide/MarketGuide/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MarketGuide;
+using Newtonsoft.Json;
+
+class Program
+{
+    
+    
+   // public class ApiResponse
+    //{
+     //   public string name { get; set; }
+        // Add other properties as needed based on your API response structure
+    //}
+    static async Task Main(string[] args)
+    {
+        // Replace the API URL with the actual endpoint you want to call
+        string token = "1xGjrWigkkC1RJfiz43Ewx";
+        string apiUrl = $"https://brapi.dev/api/quote/{args[0]}?token={token}";
+        const int Minute = 60000;
+        
+        // Create an instance of HttpClient
+        while (true) {
+            
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Send a GET request to the API
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    // Check if the response is successful (status code 200-299)
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Read the content of the response as a string
+                        string result = await response.Content.ReadAsStringAsync();
+
+                        APIResponse apiResponse = JsonConvert.DeserializeObject<APIResponse>(result);
+
+                        //Console.WriteLine(apiResponse.Results[0].Symbol);
+                        UserClauses userClauses = new UserClauses(args);
+                        userClauses.CheckClauses(apiResponse.Results[0].RegularMarketPrice);
+
+                    }
+                    else
+                    {
+                        // Handle the case where the request was not successful
+                        Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                        return;
+                    }
+                    await Task.Delay(Minute);
+                }
+                catch (Exception ex)
+                {
+                    // Handle exceptions that may occur during the request
+                    Console.WriteLine($"Exception: {ex.Message}");
+                }
+            }
+        
+        }
+    }
+}
---
+++ MarketGuide/MarketGuide/StocksINFO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketGuide
+{
+    internal class StocksINFO
+    {
+        public string Currency { get; set; }
+        public double TwoHundredDayAverage { get; set; }
+        public double TwoHundredDayAverageChange { get; set; }
+        public double TwoHundredDayAverageChangePercent { get; set; }
+        public long MarketCap { get; set; }
+        public string ShortName { get; set; }
+        public string LongName { get; set; }
+        public double RegularMarketChange { get; set; }
+        public double RegularMarketChangePercent { get; set; }
+        public DateTime RegularMarketTime { get; set; }
+        public double RegularMarketPrice { get; set; }
+        public double RegularMarketDayHigh { get; set; }
+        public string RegularMarketDayRange { get; set; }
+        public double RegularMarketDayLow { get; set; }
+        public int RegularMarketVolume { get; set; }
+        public double RegularMarketPreviousClose { get; set; }
+        public double RegularMarketOpen { get; set; }
+        public int AverageDailyVolume3Month { get; set; }
+        public int AverageDailyVolume10Day { get; set; }
+        public double FiftyTwoWeekLowChange { get; set; }
+        public double FiftyTwoWeekLowChangePercent { get; set; }
+        public string FiftyTwoWeekRange { get; set; }
+        public double FiftyTwoWeekHighChange { get; set; }
+        public double FiftyTwoWeekHighChangePercent { get; set; }
+        public double FiftyTwoWeekLow { get; set; }
+        public double FiftyTwoWeekHigh { get; set; }
+        public string Symbol { get; set; }
+        public double PriceEarnings { get; set; }
+        public double EarningsPerShare { get; set; }
+        public string LogoUrl { get; set; }
+
+    }
+}
